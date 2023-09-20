353eb5596ae1fd6e2a2f13fd6a95aa840567ee97
Halil İbrahim Direktör <hidirektor@gmail.com>
2023-09-20 03:00:24 +0300

    UI Updated
    Signed-off-by: Halil İbrahim Direktör <hidirektor@gmail.com>

--- app/src/main/java/me/t3sl4/ondergrup/Screens/OnBoard/OnBoardingFragment1.java
+++ app/src/main/java/me/t3sl4/ondergrup/Screens/OnBoard/OnBoardingFragment1.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import me.t3sl4.ondergrup.R;
@@ -21,12 +22,15 @@ public class OnBoardingFragment1 extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_on_boarding1, container, false);
+
         TextView atlaButton1 = root.findViewById(R.id.atlaButton);
         atlaButton1.setOnClickListener(v -> {
             Intent intent = new Intent(getActivity(), LoginScreen.class);
             startActivity(intent);
             getActivity().finish();
         });
+
+
 
         setOnBoardingState();
         return root;
@@ -37,5 +41,14 @@ public class OnBoardingFragment1 extends Fragment {
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putBoolean("isFirstTime", false);
         editor.apply();
+    }
+
+    private int getStatusBarHeight() {
+        int result = 0;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
     }
 }
