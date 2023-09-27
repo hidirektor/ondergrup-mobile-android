16cb1b0660502b48f663a50ccd2428a48519c62c
Halil İbrahim Direktör <hidirektor@gmail.com>
2023-09-27 19:30:23 +0300

    Support Screen

--- app/src/main/java/me/t3sl4/ondergrup/Screens/Support/SupportScreen.java
+++ app/src/main/java/me/t3sl4/ondergrup/Screens/Support/SupportScreen.java
@@ -2,15 +2,19 @@ package me.t3sl4.ondergrup.Screens.Support;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.hardware.camera2.params.ColorSpaceTransform;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import me.t3sl4.ondergrup.R;
 import me.t3sl4.ondergrup.Screens.Dashboard.DashboardEngineerScreen;
 import me.t3sl4.ondergrup.Screens.Dashboard.DashboardSysOpScreen;
 import me.t3sl4.ondergrup.Screens.Dashboard.DashboardTechnicianScreen;
 import me.t3sl4.ondergrup.Screens.Dashboard.DashboardUserScreen;
+import me.t3sl4.ondergrup.Screens.Profile.EditProfileScreen;
 import me.t3sl4.ondergrup.Screens.Profile.ProfileScreen;
 import me.t3sl4.ondergrup.Util.User.User;
 import me.t3sl4.ondergrup.Util.Util;
@@ -18,6 +22,11 @@ import me.t3sl4.ondergrup.Util.Util;
 public class SupportScreen extends AppCompatActivity {
     public Util util;
     public User receivedUser;
+
+    private ConstraintLayout homeButton;
+    private ConstraintLayout profileButton;
+    private ConstraintLayout machineButton;
+    private ConstraintLayout settingsButton;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -30,6 +39,55 @@ public class SupportScreen extends AppCompatActivity {
 
         Intent intent = getIntent();
         receivedUser = intent.getParcelableExtra("user");
+
+        homeButton = findViewById(R.id.mainConstraint);
+        profileButton = findViewById(R.id.profileConstraint);
+        machineButton = findViewById(R.id.machineConstraint);
+        settingsButton = findViewById(R.id.settingsConstraint);
+
+        homeButton.setOnClickListener(v -> {
+            geriDon();
+        });
+
+        profileButton.setOnClickListener(v -> {
+            Intent profileIntent = new Intent(SupportScreen.this, ProfileScreen.class);
+            profileIntent.putExtra("user", receivedUser);
+            startActivity(profileIntent);
+        });
+
+        machineButton.setOnClickListener(v -> {
+            //TODO
+            //Makine ekranı kodları:
+        });
+
+        settingsButton.setOnClickListener(v -> {
+            Intent settingsIntent = new Intent(SupportScreen.this, EditProfileScreen.class);
+            settingsIntent.putExtra("user", receivedUser);
+            startActivity(settingsIntent);
+            finish();
+        });
+    }
+
+    public void addMachine() {
+        //TODO
+        // Makine ekleme ekranı kodları
+    }
+
+    public void callUs() {
+        Intent callIntent = new Intent(Intent.ACTION_CALL);
+        callIntent.setData(Uri.parse("tel:+902363325050"));
+        startActivity(callIntent);
+    }
+
+    public void mailUs() {
+        //TODO
+        //mail gönderme sistemi
+    }
+
+    public void showMap() {
+        Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
+                Uri.parse("https://www.google.com/maps/place/%C3%96nder+Lift+%C3%87elik+Mak.+San.+Tic.+Ltd.+%C5%9Eti./@38.4851028,27.6519011,15z/data=!4m2!3m1!1s0x0:0x8f19e57eecad8dea?sa=X&ved=2ahUKEwi8u7iDmsuBAxWKVfEDHVNBAnIQ_BJ6BAhJEAA&ved=2ahUKEwi8u7iDmsuBAxWKVfEDHVNBAnIQ_BJ6BAhYEAg"));
+        startActivity(intent);
     }
 
     public void geriDon() {
