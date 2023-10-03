b07eba08cf6c5bdd5a5539d14853f78812d4fb4e
Halil İbrahim Direktör <hidirektor@gmail.com>
2023-10-03 08:21:01 +0300

    Little Bug Fix

--- app/src/main/java/me/t3sl4/ondergrup/Screens/Dashboard/DashboardEngineerScreen.java
+++ app/src/main/java/me/t3sl4/ondergrup/Screens/Dashboard/DashboardEngineerScreen.java
@@ -3,22 +3,16 @@ package me.t3sl4.ondergrup.Screens.Dashboard;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Environment;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.DataSource;
-import com.bumptech.glide.load.engine.GlideException;
-import com.bumptech.glide.request.RequestListener;
-import com.bumptech.glide.request.target.Target;
 
 import java.io.File;
 
@@ -93,26 +87,9 @@ public class DashboardEngineerScreen extends AppCompatActivity {
     public void setUserInfo() {
         isimSoyisim.setText(receivedUser.getNameSurname());
         String imageUrl = util.BASE_URL + util.getPhotoURLPrefix + receivedUser.getUserName() + ".jpg";
-        String imageUrl2 = util.BASE_URL + util.getPhotoURLPrefix + receivedUser.getUserName() + ".png";
 
         Glide.with(this)
-                .load(imageUrl)
-                .override(100, 100)
-                .listener(new RequestListener<Drawable>() {
-                    @Override
-                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
-                        Glide.with(DashboardEngineerScreen.this)
-                                .load(imageUrl2)
-                                .override(100, 100)
-                                .into(profilePhotoView);
-                        return true;
-                    }
-
-                    @Override
-                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
-                        return false;
-                    }
-                })
+                .load(imageUrl).override(100,100)
                 .into(profilePhotoView);
 
     }
--- app/src/main/java/me/t3sl4/ondergrup/Screens/Profile/ProfileScreen.java
+++ app/src/main/java/me/t3sl4/ondergrup/Screens/Profile/ProfileScreen.java
@@ -1,7 +1,6 @@
 package me.t3sl4.ondergrup.Screens.Profile;
 
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
@@ -9,14 +8,9 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.DataSource;
-import com.bumptech.glide.load.engine.GlideException;
-import com.bumptech.glide.request.RequestListener;
-import com.bumptech.glide.request.target.Target;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -115,29 +109,10 @@ public class ProfileScreen extends AppCompatActivity {
 
     public void setUserInfo() {
         String imageUrl = util.BASE_URL + util.getPhotoURLPrefix + receivedUser.getUserName() + ".jpg";
-        String imageUrl2 = util.BASE_URL + util.getPhotoURLPrefix + receivedUser.getUserName() + ".png";
 
         String createdAtString = receivedUser.getCreatedAt();
 
-        Glide.with(this)
-                .load(imageUrl)
-                .override(100, 100)
-                .listener(new RequestListener<Drawable>() {
-                    @Override
-                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
-                        Glide.with(ProfileScreen.this)
-                                .load(imageUrl2)
-                                .override(100, 100)
-                                .into(profilePhoto);
-                        return true;
-                    }
-
-                    @Override
-                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
-                        return false;
-                    }
-                })
-                .into(profilePhoto);
+        Glide.with(this).load(imageUrl).override(100, 100).into(profilePhoto);
         nameSurname.setText(receivedUser.getNameSurname());
         eMail.setText(receivedUser.geteMail());
         kullaniciRolu.setText(receivedUser.getRole());
