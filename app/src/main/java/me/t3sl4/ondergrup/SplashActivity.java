621ff2240535e43049dd757b72415a0629f5104c
Halil İbrahim Direktör <hidirektor@gmail.com>
2024-08-22 18:19:33 +0300

    Loading Effect

--- app/src/main/java/me/t3sl4/ondergrup/SplashActivity.java
+++ app/src/main/java/me/t3sl4/ondergrup/SplashActivity.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -19,11 +22,18 @@ import me.t3sl4.ondergrup.Util.Util;
 public class SplashActivity extends AppCompatActivity {
     private final int WAITING_TIME = 3000;
     private Dialog uyariDiyalog;
+    private ImageView logoImageView;
+    private Animation fadeIn;
+    private Animation fadeOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        logoImageView = findViewById(R.id.logo);
+        fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        fadeOut = AnimationUtils.loadAnimation(this, R.anim.fade_out);
 
         boolean isFirstTime = SharedPreferencesManager.getSharedPref("isFirstTime", this, false);
 
@@ -34,8 +44,48 @@ public class SplashActivity extends AppCompatActivity {
         if (isFirstTime) {
             setupOnboarding();
         } else {
+            startLoadingAnimation();
             redirectToMainActivity();
         }
+    }
+
+    private void startLoadingAnimation() {
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                logoImageView.startAnimation(fadeOut);
+                fadeOut.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        logoImageView.startAnimation(fadeIn);
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+                    }
+                });
+            }
+        }, 0);
+
+        fadeIn.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                logoImageView.startAnimation(fadeOut);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
     }
 
     private void setupOnboarding() {
@@ -50,17 +100,24 @@ public class SplashActivity extends AppCompatActivity {
         handler.postDelayed(() -> UserService.getProfile(SplashActivity.this, UserDataService.getUserID(SplashActivity.this), new Runnable() {
             @Override
             public void run() {
+                stopLoadingAnimation();
                 Util.redirectBasedRole(SplashActivity.this, true, uyariDiyalog);
             }
         }, () -> {
+            stopLoadingAnimation();
             refreshTokenAndRetry();
         }), WAITING_TIME);
     }
 
     private void refreshTokenAndRetry() {
-        TokenService.refreshToken(this, () -> UserService.getProfile(SplashActivity.this, UserDataService.getUserID(SplashActivity.this), () -> Util.redirectBasedRole(SplashActivity.this, true, uyariDiyalog), () -> {
+        TokenService.refreshToken(this, () -> UserService.getProfile(SplashActivity.this, UserDataService.getUserID(SplashActivity.this), () -> {
+            stopLoadingAnimation();
+            Util.redirectBasedRole(SplashActivity.this, true, uyariDiyalog);
+        }, () -> {
+            stopLoadingAnimation();
             redirectToLogin();
         }), () -> {
+            stopLoadingAnimation();
             redirectToLogin();
         });
     }
@@ -69,5 +126,9 @@ public class SplashActivity extends AppCompatActivity {
         Intent loginIntent = new Intent(SplashActivity.this, LoginScreen.class);
         startActivity(loginIntent);
         finish();
+    }
+
+    private void stopLoadingAnimation() {
+        logoImageView.clearAnimation();
     }
 }
