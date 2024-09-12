c4b2134b1d146d9b8ec089d6741a74ad321ff195
Halil İbrahim Direktör <hidirektor@gmail.com>
2024-09-12 18:08:38 +0300

    QR Scanner

--- app/src/main/java/me/t3sl4/ondergrup/UI/Screens/QR/QRScanner.java
+++ app/src/main/java/me/t3sl4/ondergrup/UI/Screens/QR/QRScanner.java
@@ -1,102 +1,23 @@
 package me.t3sl4.ondergrup.UI.Screens.QR;
 
-import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.KeyEvent;
-import android.view.View;
 
-import androidx.annotation.NonNull;
-
-import com.journeyapps.barcodescanner.CaptureManager;
-import com.journeyapps.barcodescanner.DecoratedBarcodeView;
-import com.journeyapps.barcodescanner.ViewfinderView;
+import androidx.fragment.app.FragmentActivity;
 
 import me.t3sl4.ondergrup.R;
 
-public class QRScanner extends Activity implements
-        DecoratedBarcodeView.TorchListener {
+public class QRScanner extends FragmentActivity {
 
-    private CaptureManager capture;
-    private DecoratedBarcodeView barcodeScannerView;
-    private ViewfinderView viewfinderView;
-
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_custom_scanner);
+        setContentView(R.layout.custom_barcode_scanner);
 
-        initializeComponents();
+        ScanFragment scanFragment = new ScanFragment();
 
-        barcodeScannerView.setTorchListener(this);
-
-        capture = new CaptureManager(this, barcodeScannerView);
-        capture.initializeFromIntent(getIntent(), savedInstanceState);
-        capture.setShowMissingCameraPermissionDialog(false);
-        capture.decode();
-
-        changeMaskColor(null);
-        changeLaserVisibility(true);
-    }
-
-    private void initializeComponents() {
-        barcodeScannerView = findViewById(R.id.zxing_barcode_scanner);
-
-        viewfinderView = findViewById(R.id.zxing_viewfinder_view);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        capture.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        capture.onPause();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        capture.onDestroy();
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        capture.onSaveInstanceState(outState);
-    }
-
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        return barcodeScannerView.onKeyDown(keyCode, event) || super.onKeyDown(keyCode, event);
-    }
-
-    public void changeMaskColor(View view) {
-        int color = Color.argb(65, 72, 72, 72);
-        viewfinderView.setMaskColor(color);
-    }
-
-    public void changeLaserVisibility(boolean visible) {
-        viewfinderView.setLaserVisibility(visible);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        capture.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
-
-    @Override
-    public void onTorchOn() {
-
-    }
-
-    @Override
-    public void onTorchOff() {
-
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frag_container, scanFragment)
+                .commit();
     }
 }
